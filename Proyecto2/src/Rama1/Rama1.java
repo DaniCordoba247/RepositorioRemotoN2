3bbba7d7e9ab460da5a89eea872eddbe9217159c
DANI <DANI@DESKTOP-9UM1BJ1>
2024-03-10 12:30:58 +0100

    AAA

---
+++ Proyecto2/src/Rama1/Rama1.java
@@ -0,0 +1,15 @@
+package Rama1;
+
+public class Rama1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HOLA MUNDOO!!!");
+		
+		System.out.println("COMO ESTAMOOOOS!!");
+		
+		System.out.println("Mensaje1");
+		System.out.println("Mensaje2");
+	}
+
+}
---
+++ Proyecto2/src/Rama1/Tesrt.java
@@ -0,0 +1,10 @@
+package Rama1;
+
+public class Tesrt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
